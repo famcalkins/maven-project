e016f31e59d46103f3a9fc5c3743a214746f1b67
famcalkins <fam.calkins@gmail.com>
2017-10-16 17:45:27 -0700

    changed output

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,14 +19,14 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    String someone = "World The One Being Greeted";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "World Greeting Phrase";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
